565ffac679d18899c2ec5981e5884a55c46bde91
Benjamin Confino <benjamic@uk.ibm.com>
2025-01-23 15:18:32 +0000

    Better way of getting ear libs and no need to cast to web fragment

--- dev/com.ibm.ws.container.service/src/com/ibm/ws/container/service/annocache/internal/WebAnnotationsImpl.java
+++ dev/com.ibm.ws.container.service/src/com/ibm/ws/container/service/annocache/internal/WebAnnotationsImpl.java
@@ -4,7 +4,7 @@
  * are made available under the terms of the Eclipse Public License 2.0
  * which accompanies this distribution, and is available at
  * http://www.eclipse.org/legal/epl-2.0/
- * 
+ *
  * SPDX-License-Identifier: EPL-2.0
  *
  * Contributors:
@@ -19,37 +19,25 @@ import java.util.HashMap;
 import java.util.IdentityHashMap;
 import java.util.List;
 import java.util.Map;
+import java.util.stream.Collectors;
 
-import com.ibm.ejs.ras.TraceComponent;
 import com.ibm.websphere.ras.Tr;
 import com.ibm.ws.container.service.annocache.FragmentAnnotations;
 import com.ibm.ws.container.service.annocache.WebAnnotations;
-import com.ibm.ws.container.service.app.deploy.ApplicationInfo;
-import com.ibm.ws.container.service.app.deploy.EARApplicationInfo;
+import com.ibm.ws.container.service.app.deploy.ApplicationClassesContainerInfo;
+import com.ibm.ws.container.service.app.deploy.ContainerInfo;
 import com.ibm.ws.container.service.app.deploy.WebModuleInfo;
 import com.ibm.ws.container.service.config.WebFragmentInfo;
 import com.ibm.ws.container.service.config.WebFragmentsInfo;
-
-import com.ibm.wsspi.artifact.ArtifactContainer;
-import com.ibm.wsspi.artifact.overlay.OverlayContainer;
-
 import com.ibm.wsspi.adaptable.module.Container;
-import com.ibm.wsspi.adaptable.module.Entry;
+import com.ibm.wsspi.adaptable.module.NonPersistentCache;
 import com.ibm.wsspi.adaptable.module.UnableToAdaptException;
-
 import com.ibm.wsspi.annocache.classsource.ClassSource_Aggregate;
 import com.ibm.wsspi.annocache.classsource.ClassSource_Aggregate.ScanPolicy;
 import com.ibm.wsspi.annocache.classsource.ClassSource_Factory;
 import com.ibm.wsspi.annocache.targets.AnnotationTargets_Targets;
 import com.ibm.wsspi.artifact.ArtifactContainer;
 import com.ibm.wsspi.artifact.overlay.OverlayContainer;
-
-import com.ibm.ws.container.service.app.deploy.WebModuleInfo;
-import com.ibm.ws.container.service.config.WebFragmentInfo;
-import com.ibm.ws.container.service.config.WebFragmentsInfo;
-import com.ibm.websphere.ras.Tr;
-import com.ibm.ws.container.service.annocache.FragmentAnnotations;
-import com.ibm.ws.container.service.annocache.WebAnnotations;
 
 /*
  * Web module annotation service implementation.
@@ -101,7 +89,6 @@ public class WebAnnotationsImpl extends ModuleAnnotationsImpl implements WebAnno
     //TODO discuss this, sysprop / server.xml, name, etc
     private final static String SCAN_EAR_LIBS = "com.ibm.ws.anno.ScanEarLibs";
 
-    @SuppressWarnings("unused")
     private static final boolean scanEarLibs = AccessController.doPrivileged(new PrivilegedAction<Boolean>() {
         @Override
         public Boolean run() {
@@ -112,15 +99,12 @@ public class WebAnnotationsImpl extends ModuleAnnotationsImpl implements WebAnno
     });
 
     public WebAnnotationsImpl(
-        AnnotationsAdapterImpl annotationsAdapter,
-        Container rootContainer, OverlayContainer rootOverlayContainer,
-        ArtifactContainer rootArtifactContainer, Container rootAdaptableContainer,
-        WebModuleInfo webModuleInfo) throws UnableToAdaptException {
+                              AnnotationsAdapterImpl annotationsAdapter,
+                              Container rootContainer, OverlayContainer rootOverlayContainer,
+                              ArtifactContainer rootArtifactContainer, Container rootAdaptableContainer,
+                              WebModuleInfo webModuleInfo) throws UnableToAdaptException {
 
-        super(annotationsAdapter,
-              rootContainer, rootOverlayContainer,
-              rootArtifactContainer, rootAdaptableContainer,
-              webModuleInfo);
+        super(annotationsAdapter, rootContainer, rootOverlayContainer, rootArtifactContainer, rootAdaptableContainer, webModuleInfo);
 
         this.webModuleName = webModuleInfo.getName();
         this.webFragments = rootAdaptableContainer.adapt(WebFragmentsInfo.class);
@@ -167,8 +151,8 @@ public class WebAnnotationsImpl extends ModuleAnnotationsImpl implements WebAnno
 
     //
 
-    private Map<String, WebFragmentInfo> pathToFragments;
-    private Map<WebFragmentInfo, String> fragmentToPath;
+    private final Map<String, WebFragmentInfo> pathToFragments;
+    private final Map<WebFragmentInfo, String> fragmentToPath;
 
     private String getFragmentPath(WebFragmentInfo fragment) {
         return fragmentToPath.get(fragment);
@@ -178,16 +162,16 @@ public class WebAnnotationsImpl extends ModuleAnnotationsImpl implements WebAnno
         String uniquePath = fragmentPath;
 
         int count = 1;
-        while ( pathToFragments.containsKey(uniquePath) ) {
+        while (pathToFragments.containsKey(uniquePath)) {
             uniquePath = fragmentPath + "_" + count;
             count++;
         }
-        
+
         return uniquePath;
     }
 
     private String putUniquePath(WebFragmentInfo fragment, String fragmentPath) {
-    	String uniqueFragmentPath = getUniquePath(fragmentPath);
+        String uniqueFragmentPath = getUniquePath(fragmentPath);
 
         fragmentToPath.put(fragment, uniqueFragmentPath);
         pathToFragments.put(uniqueFragmentPath, fragment);
@@ -199,17 +183,17 @@ public class WebAnnotationsImpl extends ModuleAnnotationsImpl implements WebAnno
 
     @Override
     protected void addInternalToClassSource() {
-    	String methodName = "addInternalToClassSource";
+        String methodName = "addInternalToClassSource";
 
-        if ( rootClassSource == null ) {
+        if (rootClassSource == null) {
             return;
         }
 
         ClassSource_Factory classSourceFactory = getClassSourceFactory();
-        if ( classSourceFactory == null ) {
+        if (classSourceFactory == null) {
             return;
         }
-        
+
         List<Container> earLibs = scanEarLibs ? getEarLibs() : new ArrayList<Container>();
 
         // The classes folder is processed as if it were a fragment item.
@@ -226,16 +210,16 @@ public class WebAnnotationsImpl extends ModuleAnnotationsImpl implements WebAnno
         // excluded element.  Less class information is used from excluded
         // fragments than is used from partial fragments.
 
-        for ( WebFragmentInfo nextFragment : getOrderedItems() ) {
+        for (WebFragmentInfo nextFragment : getOrderedItems()) {
             String nextUri = nextFragment.getLibraryURI();
             Container nextContainer = nextFragment.getFragmentContainer();
-            
+
             //Defensive programming to avoid duplicates
             earLibs.remove(nextContainer);
 
             boolean nextIsMetadataComplete;
             ScanPolicy nextPolicy;
-            if ( nextFragment.isSeedFragment() ) {
+            if (nextFragment.isSeedFragment()) {
                 nextPolicy = ClassSource_Aggregate.ScanPolicy.SEED;
                 nextIsMetadataComplete = false;
             } else {
@@ -243,20 +227,20 @@ public class WebAnnotationsImpl extends ModuleAnnotationsImpl implements WebAnno
                 nextIsMetadataComplete = true;
             }
 
-            if ( tc.isDebugEnabled() ) {
+            if (tc.isDebugEnabled()) {
                 Tr.debug(tc, methodName + ": Fragment [ " + nextFragment + " ]");
                 Tr.debug(tc, methodName + ": URI [ " + nextUri + " ]");
                 Tr.debug(tc, methodName + ": Container [ " + nextContainer + " ]");
-                Tr.debug(tc, methodName + ": Metadata Complete [ " + nextIsMetadataComplete + " ]"); 
+                Tr.debug(tc, methodName + ": Metadata Complete [ " + nextIsMetadataComplete + " ]");
             }
 
             String nextPrefix;
-            if ( nextUri.equals("WEB-INF/classes") ) {
+            if (nextUri.equals("WEB-INF/classes")) {
                 // The expectation is that the supplied container is twice nested
                 // local child of the module container.
                 nextContainer = nextContainer.getEnclosingContainer().getEnclosingContainer();
                 nextPrefix = "WEB-INF/classes/";
-                if ( tc.isDebugEnabled() ) {
+                if (tc.isDebugEnabled()) {
                     Tr.debug(tc, methodName + ": Assigned Prefix [ " + nextPrefix + " ]");
                 }
             } else {
@@ -264,40 +248,34 @@ public class WebAnnotationsImpl extends ModuleAnnotationsImpl implements WebAnno
             }
 
             String nextPath = getContainerPath(nextContainer);
-            if ( nextPath == null ) {
+            if (nextPath == null) {
                 return; // FFDC in 'getContainerPath'
             }
             nextPath = putUniquePath(nextFragment, nextPath);
-            if ( tc.isDebugEnabled() ) {
+            if (tc.isDebugEnabled()) {
                 Tr.debug(tc, methodName + ": Fragment [ " + nextFragment + " ]");
                 Tr.debug(tc, methodName + ": Path [ " + nextPath + " ]");
             }
 
-            if ( !addContainerClassSource(nextPath, nextContainer, nextPrefix, nextPolicy) ) {
+            if (!addContainerClassSource(nextPath, nextContainer, nextPrefix, nextPolicy)) {
                 return; // FFDC in 'addContainerClassSource'
             }
         }
-        
+
         //TODO property name, should it be a server.xml property instead, etc. Talk with Tom
         if (scanEarLibs) {
-            try {
-                for (Container c : earLibs) {
-                    WebFragmentInfo nextFragmentInfo = c.adapt(WebFragmentInfo.class);
-                    String nextPath = putUniquePath(nextFragmentInfo, getContainerPath(c));
-                    if (!addContainerClassSource(nextPath, c, ClassSource_Aggregate.ScanPolicy.SEED)) { //TODO should this be SEED?
-                        return; // FFDC in 'addContainerClassSource'
-                    }
+            for (Container c : earLibs) {
+                if (!addContainerClassSource(c.getPath(), c, ClassSource_Aggregate.ScanPolicy.SEED)) { //TODO should this be SEED?
+                    return; // FFDC in 'addContainerClassSource'
                 }
-            } catch (UnableToAdaptException e) {
-                //TODO, ask Tom what he wants to happen here
             }
         }
 
-        for ( WebFragmentInfo nextFragment : getExcludedItems() ) {
-        	String nextUri = nextFragment.getLibraryURI();
+        for (WebFragmentInfo nextFragment : getExcludedItems()) {
+            String nextUri = nextFragment.getLibraryURI();
             Container nextContainer = nextFragment.getFragmentContainer();
 
-            if ( tc.isDebugEnabled() ) {
+            if (tc.isDebugEnabled()) {
                 Tr.debug(tc, methodName + ": Fragment [ " + nextFragment + " ]");
                 Tr.debug(tc, methodName + ": URI [ " + nextUri + " ]");
                 Tr.debug(tc, methodName + ": Container [ " + nextContainer + " ]");
@@ -305,45 +283,34 @@ public class WebAnnotationsImpl extends ModuleAnnotationsImpl implements WebAnno
             }
 
             String nextPath = getContainerPath(nextContainer);
-            if ( nextPath == null ) {
+            if (nextPath == null) {
                 return; // FFDC in 'getContainerPath'
             }
             nextPath = putUniquePath(nextFragment, nextPath);
 
-            if ( tc.isDebugEnabled() ) {
+            if (tc.isDebugEnabled()) {
                 Tr.debug(tc, methodName + ": Fragment [ " + nextFragment + " ]");
                 Tr.debug(tc, methodName + ": Path [ " + nextPath + " ]");
             }
 
-            if ( !addContainerClassSource(nextPath, nextContainer, ClassSource_Aggregate.ScanPolicy.EXCLUDED) ) {
+            if (!addContainerClassSource(nextPath, nextContainer, ClassSource_Aggregate.ScanPolicy.EXCLUDED)) {
                 return; // FFDC in 'addContainerClassSource'
             }
         }
     }
-    
+
     private List<Container> getEarLibs() {
-        List<Container> earLibs = new ArrayList<Container>();
 
-        ApplicationInfo appInfo = this.moduleInfo.getApplicationInfo();
-        EARApplicationInfo earAppInfo = null;
-
-        if (appInfo instanceof EARApplicationInfo) {
-            earAppInfo = (EARApplicationInfo) appInfo;
-            Container libContainer = earAppInfo.getLibraryDirectoryContainer();
-
-            for (Entry entry : libContainer) {
-                if (entry.getName().endsWith(".jar")) {
-                    try {
-                        Container lib = entry.adapt(Container.class);
-                        earLibs.add(lib);
-                    } catch (UnableToAdaptException ex) {
-                        // Not really a jar archive, just a poorly named file
-                        if (TraceComponent.isAnyTracingEnabled() && tc.isDebugEnabled())
-                            Tr.debug(tc, "processLibraryJarPersistenceXml: ignoring " + entry.getName(), ex);
-                    }
-                }
-            }
+        NonPersistentCache cache;
+        try {
+            cache = getAppContainer().adapt(NonPersistentCache.class);
+            // 'adapt' throws UnableToAdaptException
+        } catch (UnableToAdaptException e) {
+            return null; // FFDC
         }
+
+        ApplicationClassesContainerInfo appClassesInfo = (ApplicationClassesContainerInfo) cache.getFromCache(ApplicationClassesContainerInfo.class);
+        List<Container> earLibs = appClassesInfo.getLibraryClassesContainerInfo().stream().map(ContainerInfo::getContainer).collect(Collectors.toList());
 
         return earLibs;
     }
@@ -353,10 +320,9 @@ public class WebAnnotationsImpl extends ModuleAnnotationsImpl implements WebAnno
     @Override
     public FragmentAnnotations getFragmentAnnotations(WebFragmentInfo fragment) {
         AnnotationTargets_Targets useTargets = getTargets();
-        if ( useTargets == null ) {
+        if (useTargets == null) {
             return null;
         }
-        return new FragmentAnnotationsImpl( useTargets, getFragmentPath(fragment) );
+        return new FragmentAnnotationsImpl(useTargets, getFragmentPath(fragment));
     }
 }
-
