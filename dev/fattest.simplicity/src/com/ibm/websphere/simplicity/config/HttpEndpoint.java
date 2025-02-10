eabf9f789c4217e2ea5dfdf9c6c0f04848c6c51d
Volodymyr Siedlecki <volosied@gmail.com>
2025-02-10 10:38:33 -0500

    Create HttpOptions server config in fattest.simplicity

--- dev/fattest.simplicity/src/com/ibm/websphere/simplicity/config/HttpEndpoint.java
+++ dev/fattest.simplicity/src/com/ibm/websphere/simplicity/config/HttpEndpoint.java
@@ -23,6 +23,8 @@ import javax.xml.bind.annotation.XmlElement;
  */
 public class HttpEndpoint extends ConfigElement {
 
+    @XmlElement(name = "httpOptions")
+    private HttpOptions httpOptions;
     @XmlElement(name = "tcpOptions")
     private TcpOptions tcpOptions;
     @XmlElement(name = "sslOptions")
@@ -37,6 +39,16 @@ public class HttpEndpoint extends ConfigElement {
     private String httpPort;
     private String httpsPort;
     private String protocolVersion;
+
+    /**
+     * @return HTTP options for this configuration
+     */
+    public HttpOptions getHttpOptions() {
+        if (this.httpOptions == null) {
+            this.httpOptions = new HttpOptions();
+        }
+        return this.httpOptions;
+    }
 
     /**
      * @return TCP options for this configuration
@@ -189,6 +201,8 @@ public class HttpEndpoint extends ConfigElement {
             buf.append(tcpOptions.toString());
         if (this.sslOptions != null)
             buf.append(sslOptions.toString());
+        if (this.httpOptions != null)
+            buf.append(httpOptions.toString());
         if (this.sameSite != null)
             buf.append(sameSite.toString());
         if (samesiteRef != null)
