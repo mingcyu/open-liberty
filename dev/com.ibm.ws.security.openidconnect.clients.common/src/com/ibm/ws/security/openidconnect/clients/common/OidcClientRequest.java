23e60f46825862ba20b77561b177415c4abf764a
Wilalberto Rodriguez <wrodrig@us.ibm.com>
2025-03-13 10:08:45 -0500

    Instead of putting the thread client id in the oidc classes, try putting it in the WebProviderAuthenticator

--- dev/com.ibm.ws.security.openidconnect.clients.common/src/com/ibm/ws/security/openidconnect/clients/common/OidcClientRequest.java
+++ dev/com.ibm.ws.security.openidconnect.clients.common/src/com/ibm/ws/security/openidconnect/clients/common/OidcClientRequest.java
@@ -63,7 +63,6 @@ public class OidcClientRequest extends OidcCommonClientRequest {
 
     protected String tokenType = TYPE_ID_TOKEN;
     protected String tokenTypeNoSpace = Constants.TOKEN_TYPE_ID_TOKEN;
-    public static ThreadLocal<String> threadClientID = new ThreadLocal<String>();
 
     OidcClientRequest() {
         // for FAT, make its scope package only
@@ -79,12 +78,9 @@ public class OidcClientRequest extends OidcCommonClientRequest {
         this.request = request;
         this.response = response;
         OidcClientUtil.setReferrerURLCookieHandler(referrerURLCookieHandler);
-                 
         clientConfigId = convergedClientConfig.getId();
         authnSessionDisabled = convergedClientConfig.isAuthnSessionDisabled_propagation();
         inboundValue = convergedClientConfig.getInboundPropagation();
-        
-        setThreadClientId(this.oidcClientConfig.getClientId());
 
         request.setAttribute(AUTHN_SESSION_DISABLED, Boolean.valueOf(authnSessionDisabled));
         request.setAttribute(INBOUND_PROPAGATION_VALUE, inboundValue);
@@ -382,14 +378,6 @@ public class OidcClientRequest extends OidcCommonClientRequest {
     @Override
     public boolean disableIssChecking() {
         return oidcClientConfig.disableIssChecking();
-    }
-
-    private void setThreadClientId(String clientID) {
-        threadClientID.set(clientID);
-    }
-
-    public static String getThreadClientId() {
-        return threadClientID.get();
     }
 
 }
