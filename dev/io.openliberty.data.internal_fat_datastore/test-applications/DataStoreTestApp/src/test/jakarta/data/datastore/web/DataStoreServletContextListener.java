2035a6da052c829a983c4f5386906a6ed23fa6d4
Nathan Rauh <nathan.rauh@us.ibm.com>
2025-03-26 15:00:15 -0500

    Issue #30879 disabled test for ServletContextListener scenario

---
+++ dev/io.openliberty.data.internal_fat_datastore/test-applications/DataStoreTestApp/src/test/jakarta/data/datastore/web/DataStoreServletContextListener.java
@@ -0,0 +1,44 @@
+/*******************************************************************************
+ * Copyright (c) 2025 IBM Corporation and others.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License 2.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-2.0/
+ *
+ * SPDX-License-Identifier: EPL-2.0
+ *
+ * Contributors:
+ *     IBM Corporation - initial API and implementation
+ *******************************************************************************/
+package test.jakarta.data.datastore.web;
+
+import jakarta.inject.Inject;
+import jakarta.servlet.ServletContextEvent;
+import jakarta.servlet.ServletContextListener;
+import jakarta.servlet.annotation.WebListener;
+
+/**
+ * Servlet context listener that uses Jakarta Data to pre-populate the table
+ * that is used by the ServerDSResRefRepo entity.
+ */
+@WebListener
+public class DataStoreServletContextListener implements ServletContextListener {
+
+    @Inject
+    ServerDSResRefRepo serverDSResRefRepo;
+
+    @Override
+    public void contextDestroyed(ServletContextEvent event) {
+    }
+
+    @Override
+    public void contextInitialized(ServletContextEvent event) {
+        System.out.println("DataStoreServletContextListener.contextInitialized:" +
+                           " populate tables for " + // serverDSResRefRepo.toString());
+                           " TODO: serverDSResRefRepo.toString()");
+
+        // TODO add code to write values here
+        // and include test in servlet to read them
+    }
+
+}
