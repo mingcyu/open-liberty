1758a7e560b347f4b32c6a8d83a863b5b7b453e9
Benjamin Confino <benjamic@uk.ibm.com>
2025-04-02 20:06:38 +0100

    undo removal of second copyright and add missing copyright

--- dev/com.ibm.ws.container.service/src/com/ibm/ws/container/service/annocache/internal/WebEJBAnnotationsImpl.java
+++ dev/com.ibm.ws.container.service/src/com/ibm/ws/container/service/annocache/internal/WebEJBAnnotationsImpl.java
@@ -1,3 +1,12 @@
+/*******************************************************************************
+ * Copyright (c) 2025 IBM Corporation and others.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License 2.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-2.0/
+ *
+ * SPDX-License-Identifier: EPL-2.0
+ *******************************************************************************/
 package com.ibm.ws.container.service.annocache.internal;
 
 import java.util.HashMap;
@@ -21,10 +30,10 @@ public class WebEJBAnnotationsImpl extends WebAnnotationsImpl {
 
     protected WebEJBAnnotationsImpl(WebAnnotationsImpl webAnnotations) throws UnableToAdaptException {
         super(webAnnotations, WEB_EJB_CAT_NAME);
-                
+
         this.webAnnotations = webAnnotations;
     }
-    
+
     protected final WebAnnotationsImpl webAnnotations;
 
     public WebAnnotationsImpl getWebAnnotations() {
@@ -34,22 +43,22 @@ public class WebEJBAnnotationsImpl extends WebAnnotationsImpl {
     /**
      * Override: Create a root class source which is very
      * similar to the root class source of the base web annotations.
-     * 
+     *
      * Use the same application name and module name as the base
      * web annotations.
-     * 
+     *
      * Use the alternate "web_ejb" category name.
-     * 
+     *
      * Use the same intern map as the base web annotations.
-     * 
+     *
      * Use the same options as the base web annotations.
-     * 
+     *
      * @return A new root class source.
      */
     @Override
     protected ClassSource_Aggregate createRootClassSource() {
         ClassSource_Factory classSourceFactory = getClassSourceFactory();
-        if ( classSourceFactory == null ) {
+        if (classSourceFactory == null) {
             return null;
         }
 
@@ -62,7 +71,7 @@ public class WebEJBAnnotationsImpl extends WebAnnotationsImpl {
         // do not add extra libraries.
 
         WebAnnotationsImpl useWebAnnotations = getWebAnnotations();
-        
+
         String useAppName = useWebAnnotations.getAppName();
         String useModName = useWebAnnotations.getModName();
 
@@ -71,32 +80,32 @@ public class WebEJBAnnotationsImpl extends WebAnnotationsImpl {
         ClassSource_Aggregate webClassSource = useWebAnnotations.getClassSource();
         ClassSource_Options webOptions = webClassSource.getOptions();
 
-        if ( tc.isDebugEnabled() ) {
+        if (tc.isDebugEnabled()) {
             String prefix = CLASS_NAME + ".createRootClassSource:";
             Tr.debug(tc, prefix + " Class source" +
-                                  " app [ " + useAppName + " ]" +
-                                  " mod [ " + useModName + " ]" +
-                                  " mod cat [ " + useModCatName + " ]");
+                         " app [ " + useAppName + " ]" +
+                         " mod [ " + useModName + " ]" +
+                         " mod cat [ " + useModCatName + " ]");
             Tr.debug(tc, prefix + " Scan threads [ " + webOptions.getScanThreads() + " ]");
         }
 
         try {
             return classSourceFactory.createAggregateClassSource(
-                webClassSource.getInternMap(),
-                useAppName, useModName, useModCatName,
-                webOptions); // throws ClassSource_Exception
-        } catch ( ClassSource_Exception e ) {
+                                                                 webClassSource.getInternMap(),
+                                                                 useAppName, useModName, useModCatName,
+                                                                 webOptions); // throws ClassSource_Exception
+        } catch (ClassSource_Exception e) {
             return null; // FFDC
         }
     }
-    
+
     /**
      * Add internal containers to the class source.
-     * 
+     *
      * This replicates the class source of the base web module, adding the
      * same containers in the same order, and with the same prefix and scan policies.
-     * 
-     * Except, the extra containers are not added.  These are not scanned for EJB metadata.
+     *
+     * Except, the extra containers are not added. These are not scanned for EJB metadata.
      */
     @Override
     protected void addInternalToClassSource() {
@@ -105,21 +114,21 @@ public class WebEJBAnnotationsImpl extends WebAnnotationsImpl {
         }
 
         boolean isDebug = tc.isDebugEnabled();
-        String prefix = ( isDebug ? CLASS_NAME + ".addInternalToClassSource" : null );
+        String prefix = (isDebug ? CLASS_NAME + ".addInternalToClassSource" : null);
 
         WebAnnotationsImpl useWebAnnotations = getWebAnnotations();
         ClassSource_Aggregate webSource = useWebAnnotations.getClassSource();
         List<? extends ClassSource> webLeafSources = webSource.getClassSources();
 
-        Map<String, ClassSource> webLeafSourceMap = new HashMap<>( webLeafSources.size() );
-        for ( ClassSource webLeafSource : webLeafSources ) {
-            webLeafSourceMap.put( webLeafSource.getName(), webLeafSource );
+        Map<String, ClassSource> webLeafSourceMap = new HashMap<>(webLeafSources.size());
+        for (ClassSource webLeafSource : webLeafSources) {
+            webLeafSourceMap.put(webLeafSource.getName(), webLeafSource);
         }
 
-        for ( String internalPath : useWebAnnotations.getInternalContainers() ) {
+        for (String internalPath : useWebAnnotations.getInternalContainers()) {
             ClassSource webLeafSource = webLeafSourceMap.get(internalPath);
-            if ( !(webLeafSource instanceof ClassSource_MappedContainer) ) {
-                if ( isDebug ) {
+            if (!(webLeafSource instanceof ClassSource_MappedContainer)) {
+                if (isDebug) {
                     Tr.debug(tc, prefix + " Skip non-container source [ " + internalPath + " ]");
                 }
                 return; // Unexpected
@@ -130,7 +139,7 @@ public class WebEJBAnnotationsImpl extends WebAnnotationsImpl {
             String nextPrefix = typedSource.getEntryPrefix();
             ClassSource_Aggregate.ScanPolicy nextPolicy = webSource.getScanPolicy(webLeafSource);
 
-            if ( isDebug ) {
+            if (isDebug) {
                 Tr.debug(tc, prefix + " Container source [ " + internalPath + " ] [ " + nextPrefix + " ] [ " + nextPolicy + " ]");
             }
 
@@ -138,7 +147,7 @@ public class WebEJBAnnotationsImpl extends WebAnnotationsImpl {
 
             // TODO: Can the leaf class sources be shared?
 
-            if ( !addContainerClassSource(internalPath, nextContainer, nextPrefix, nextPolicy) ) { 
+            if (!addContainerClassSource(internalPath, nextContainer, nextPrefix, nextPolicy)) {
                 return; // FFDC in 'addContainerClassSource'
             }
         }
