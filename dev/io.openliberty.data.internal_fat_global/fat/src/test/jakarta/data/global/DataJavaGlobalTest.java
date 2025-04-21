1b14bfd606c80ed57d86af4721075c2546d633b3
Nathan Rauh <nathan.rauh@us.ibm.com>
2025-04-21 14:37:53 -0500

    Issues #28830 test of repository with shared java:global DataSource continuing to be usable after another application that uses it stops

--- dev/io.openliberty.data.internal_fat_global/fat/src/test/jakarta/data/global/DataJavaGlobalTest.java
+++ dev/io.openliberty.data.internal_fat_global/fat/src/test/jakarta/data/global/DataJavaGlobalTest.java
@@ -15,6 +15,8 @@ package test.jakarta.data.global;
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.fail;
 
+import java.util.Set;
+
 import javax.json.JsonObject;
 
 import org.jboss.shrinkwrap.api.ShrinkWrap;
@@ -25,6 +27,7 @@ import org.junit.Test;
 import org.junit.runner.RunWith;
 
 import com.ibm.websphere.simplicity.ShrinkHelper;
+import com.ibm.websphere.simplicity.config.ServerConfiguration;
 
 import componenttest.annotation.MinimumJavaLevel;
 import componenttest.annotation.Server;
@@ -106,7 +109,27 @@ public class DataJavaGlobalTest extends FATServletClient {
 
     @AfterClass
     public static void tearDown() throws Exception {
-        server.stopServer(EXPECTED_ERROR_MESSAGES);
+        try {
+            // Stop an application that has a repository that uses a DataSource
+            // that is defined in a different application
+            ServerConfiguration config = server.getServerConfiguration();
+            config.getApplications().removeBy("location", "DataGlobalWebApp.war");
+            server.setMarkToEndOfLog();
+            server.updateServerConfiguration(config);
+            server.waitForConfigUpdateInLogUsingMark(Set.of(),
+                                                     "CWWKZ0009I.*DataGlobalWebApp");
+
+            // Verify that the repository in the remaining application can continue
+            // to be used.
+            String path = "/DataGlobalRestApp/data/reminder/id/3";
+            JsonObject json = new HttpRequest(server, path).run(JsonObject.class);
+
+            String found = "found: " + json;
+            assertEquals(found, 3, json.getInt("id"));
+            assertEquals(found, "Do this third.", json.getString("message"));
+        } finally {
+            server.stopServer(EXPECTED_ERROR_MESSAGES);
+        }
     }
 
     /**
@@ -117,10 +140,10 @@ public class DataJavaGlobalTest extends FATServletClient {
         String path = "/DataGlobalRestApp/data/reminder/id/1";
         JsonObject json = new HttpRequest(server, path).run(JsonObject.class);
 
-        String err = "unexpected response: " + json;
+        String found = "found: " + json;
 
-        assertEquals(err, 1, json.getInt("id"));
-        assertEquals(err, "Do this first.", json.getString("message"));
+        assertEquals(found, 1, json.getInt("id"));
+        assertEquals(found, "Do this first.", json.getString("message"));
     }
 
     /**
