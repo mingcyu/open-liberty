33ef49c1227257938d40e7f011f802eff85c7340
Thomas Watson <tjwatson@us.ibm.com>
2025-04-29 15:04:23 -0500

    Add Spring data/JPA integration test for Spring Framework V5 app

---
+++ dev/com.ibm.ws.springboot.fat20.data.app/src/main/java/com/ibm/ws/springboot/support/version20/test/data/app/EmployeeConfiguration.java
@@ -0,0 +1,22 @@
+/*******************************************************************************
+ * Copyright (c) 2025 IBM Corporation and others.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License 2.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-2.0/
+ *
+ * SPDX-License-Identifier: EPL-2.0
+ *
+ *******************************************************************************/
+package com.ibm.ws.springboot.support.version20.test.data.app;
+
+import com.ibm.ws.springboot.support.version20.test.data.app.employee.Employee;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+
+@Configuration(proxyBeanMethods = false)
+@EnableJpaRepositories(basePackageClasses = Employee.class, entityManagerFactoryRef = "employeeEntityManagerFactory")
+public class EmployeeConfiguration {
+
+}
