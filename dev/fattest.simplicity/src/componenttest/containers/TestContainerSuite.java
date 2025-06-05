47cd590975453d7ba9b367605861407d19c07d6e
Kyle Aure <KyleJAure@gmail.com>
2025-06-05 16:59:54 -0500

    refactor: vncrecorder image from ghcr.io

--- dev/fattest.simplicity/src/componenttest/containers/TestContainerSuite.java
+++ dev/fattest.simplicity/src/componenttest/containers/TestContainerSuite.java
@@ -222,6 +222,8 @@ public class TestContainerSuite {
         //Always use internal testcontainer images from alternative sources (where possible)
         tcProps.setProperty("tinyimage.container.image", "public.ecr.aws/docker/library/alpine:3.17");
         tcProps.setProperty("ryuk.container.image", "ghcr.io/testcontainers/ryuk:0.12.0");
+        tcProps.setProperty("vncrecorder.container.image", "ghcr.io/testcontainers/vnc-recorder:1.4.0");
+//        tcProps.setProperty("sshd.container.image", "ghcr.io/testcontainers/sshd:1.3.0"); TODO
 
         try {
             tcProps.store(new FileOutputStream(configSource.toFile()), "Modified by FAT framework");
