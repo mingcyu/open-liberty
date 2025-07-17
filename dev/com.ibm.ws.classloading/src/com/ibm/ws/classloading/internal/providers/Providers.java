8d03a35425d5852b2a7656b17d08d7568cba74d2
Thomas Watson <tjwatson@us.ibm.com>
2025-07-17 11:06:02 -0500

    Add options for patch library and library delegation before app class path
    Adds config attribute called libraryDelegation to the global
    classloading config that has options beforeApp and afterApp where
    afterApp is the default behavior today.  When using beforeApp
    option the library delegation will happen before the application
    class path and before parent delegation.  The library loader will
    also be configured as parentLast delegation.
    
    Adds a config attribute on classloader config to called
    patchLibraryRef which is similar to privateLibraryRef.  Both
    options take the library class path content and place it
    on the local class path of the application.  A patchLibrary
    will add the content to the beginning of the local class path.
    A privateLibrary adds the content to the end of the local class path

--- dev/com.ibm.ws.classloading/src/com/ibm/ws/classloading/internal/providers/Providers.java
+++ dev/com.ibm.ws.classloading/src/com/ibm/ws/classloading/internal/providers/Providers.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2014,2019 IBM Corporation and others.
+ * Copyright (c) 2014, 2025 IBM Corporation and others.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License 2.0
  * which accompanies this distribution, and is available at
@@ -27,6 +27,7 @@ import org.osgi.framework.ServiceReference;
 
 import com.ibm.websphere.ras.Tr;
 import com.ibm.websphere.ras.TraceComponent;
+import com.ibm.ws.classloading.configuration.GlobalClassloadingConfiguration.LibraryPrecedence;
 import com.ibm.ws.classloading.internal.DeclaredApiAccess;
 import com.ibm.ws.classloading.internal.LibertyLoader;
 import com.ibm.ws.classloading.internal.util.BlockingList;
@@ -40,6 +41,15 @@ import com.ibm.wsspi.kernel.service.utils.FilterUtils;
 import com.ibm.wsspi.library.Library;
 
 public class Providers {
+    public static final class LibraryInfo {
+        public final LibertyLoader loader;
+        public final LibraryPrecedence precedence;
+        LibraryInfo(LibertyLoader loader, LibraryPrecedence precedence) {
+            this.loader = loader;
+            this.precedence = precedence;
+        }
+    }
+
     static final TraceComponent tc = Tr.register(Providers.class);
     static BundleContext bundleContext;
 
@@ -49,10 +59,15 @@ public class Providers {
     }
 
     public static List<Library> getPrivateLibraries(ClassLoaderConfiguration config) {
-        List<String> privateLibraries = config.getSharedLibraries();
-        if (privateLibraries == null || privateLibraries.isEmpty()) {
+        return getLibraries(config.getId().getId(), config.getSharedLibraries());
+    }
+    public static List<Library> getPatchLibraries(ClassLoaderConfiguration config) {
+        return getLibraries(config.getId().getId(), config.getPatchLibraries());
+    }
+    public static List<Library> getLibraries(String ownerId, List<String> libraryIds) {
+        if (libraryIds == null || libraryIds.isEmpty()) {
             if (tc.isDebugEnabled())
-                Tr.debug(tc, "RETURN (privateLibraries == null || privateLibraries.isEmpty())");
+                Tr.debug(tc, "RETURN (libraryIds == null || libraryIds.isEmpty())");
             return Collections.emptyList();
         }
 
@@ -63,11 +78,11 @@ public class Providers {
             return Collections.emptyList();
         }
 
-        GetLibraries getLibraries = new GetLibraries(config.getId().getId());
-        return BlockingListMaker.defineList().waitFor(10, SECONDS).fetchElements(getLibraries).listenForElements(getLibraries).log(LOGGER).useKeys(privateLibraries).make();
+        GetLibraries getLibraries = new GetLibraries(ownerId);
+        return BlockingListMaker.defineList().waitFor(10, SECONDS).fetchElements(getLibraries).listenForElements(getLibraries).log(LOGGER).useKeys(libraryIds).make();
     }
 
-    public static List<LibertyLoader> getCommonLibraryLoaders(ClassLoaderConfiguration config, DeclaredApiAccess apiAccess) {
+    public static List<Providers.LibraryInfo> getCommonLibraryLoaders(ClassLoaderConfiguration config, DeclaredApiAccess apiAccess, LibraryPrecedence precedence) {
         List<String> commonLibIds = config.getCommonLibraries();
         if (commonLibIds == null || commonLibIds.isEmpty()) {
             if (tc.isDebugEnabled())
@@ -86,11 +101,11 @@ public class Providers {
 
         // this list will try to retrieve the libraries on demand
         // and it will block until they are available
-        GetLibraryLoaders getLibraryLoaders = new GetLibraryLoaders(config.getId().getId(), gwApis);
+        GetLibraryLoaders getLibraryLoaders = new GetLibraryLoaders(config.getId().getId(), gwApis, precedence);
         return BlockingListMaker.defineList().waitFor(10, SECONDS).fetchElements(getLibraryLoaders).listenForElements(getLibraryLoaders).log(LOGGER).useKeys(commonLibIds).make();
     }
 
-    public static List<LibertyLoader> getProviderLoaders(ClassLoaderConfiguration config, DeclaredApiAccess apiAccess) {
+    public static List<Providers.LibraryInfo> getProviderLoaders(ClassLoaderConfiguration config, DeclaredApiAccess apiAccess) {
         final String methodName = "getProviderLoaders(): ";
         List<String> providerIds = config.getClassProviders();
         if (providerIds == null || providerIds.isEmpty()) {
@@ -115,8 +130,8 @@ public class Providers {
     }
 
     @SuppressWarnings("unchecked")
-    public static Iterable<LibertyLoader> getDelegateLoaders(ClassLoaderConfiguration config, DeclaredApiAccess apiAccess) {
-        return new CompositeIterable<LibertyLoader>(getCommonLibraryLoaders(config, apiAccess), getProviderLoaders(config, apiAccess));
+    public static Iterable<Providers.LibraryInfo> getDelegateLoaders(ClassLoaderConfiguration config, DeclaredApiAccess apiAccess, LibraryPrecedence precedence) {
+        return new CompositeIterable<Providers.LibraryInfo>(getCommonLibraryLoaders(config, apiAccess, precedence), getProviderLoaders(config, apiAccess));
     }
 
     /**
