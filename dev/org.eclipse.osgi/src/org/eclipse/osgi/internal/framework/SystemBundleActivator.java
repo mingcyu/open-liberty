55d8bcc5b8b1a7cdb29f8ec1e6f3b7911c464f04
Thomas Watson <tjwatson@us.ibm.com>
2025-08-08 08:36:46 -0500

    Overlay for Equinox PR 1123

--- dev/org.eclipse.osgi/src/org/eclipse/osgi/internal/framework/SystemBundleActivator.java
+++ dev/org.eclipse.osgi/src/org/eclipse/osgi/internal/framework/SystemBundleActivator.java
@@ -254,7 +254,12 @@ public class SystemBundleActivator implements BundleActivator {
 						e.getMessage(), e);
 			}
 		}
-
+		try {
+			plurl.uninstall();
+		} catch (Exception e) {
+			bundle.getEquinoxContainer().getLogServices().log(EquinoxContainer.NAME, FrameworkLogEntry.ERROR,
+					e.getMessage(), e);
+		}
 		// unregister services
 		for (ServiceRegistration<?> registration : registrations)
 			registration.unregister();
@@ -291,4 +296,3 @@ public class SystemBundleActivator implements BundleActivator {
 		registrations.add(context.registerService(serviceClass, service, properties));
 	}
 }
-
