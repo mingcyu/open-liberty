96dfd25e0149c2ad1b24572772b2e6cf2185fe8a
Nathan Rauh <nathan.rauh@us.ibm.com>
2025-08-14 16:02:58 -0500

    Create test bucket, initially empty

---
+++ dev/com.ibm.ws.concurrent_fat_no_vt/test-applications/ConcurrentNoVTWeb/src/test/concurrent/no/vt/web/ConcurrentVTDisabledServlet.java
@@ -0,0 +1,55 @@
+/*******************************************************************************
+ * Copyright (c) 2025 IBM Corporation and others.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License 2.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-2.0/
+ *
+ * SPDX-License-Identifier: EPL-2.0
+ *
+ * Contributors:
+ *     IBM Corporation - initial API and implementation
+ *******************************************************************************/
+package test.concurrent.no.vt.web;
+
+import static org.junit.Assert.assertEquals;
+
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.TimeUnit;
+
+import jakarta.servlet.ServletConfig;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+
+import org.junit.Test;
+
+import componenttest.app.FATServlet;
+
+@SuppressWarnings("serial")
+@WebServlet("/*")
+public class ConcurrentVTDisabledServlet extends FATServlet {
+
+    // Maximum number of nanoseconds to wait for a task to finish.
+    private static final long TIMEOUT_NS = TimeUnit.MINUTES.toNanos(2);
+
+    private ExecutorService unmanagedThreads;
+
+    @Override
+    public void destroy() {
+        unmanagedThreads.shutdownNow();
+    }
+
+    @Override
+    public void init(ServletConfig config) throws ServletException {
+        unmanagedThreads = Executors.newFixedThreadPool(5);
+    }
+
+    /**
+     */
+    @Test
+    public void testPass() throws Exception {
+
+        assertEquals(true, true);
+    }
+}
